ac6859e6798661f26b83879593e9ceffc012672f
jeremy2212landa <jeremy2212landa@gmail.com>
2021-10-05 10:59:57 -0400

    commit

---
+++ models/CP_Model.php
@@ -0,0 +1,57 @@
+<?php
+class CP_Model extends Model {
+
+
+	public function create( $cp_data = array() ) {
+		foreach ($cp_data as $key => $value) {
+			$$key = $value;
+		}
+
+		$this->query = "INSERT INTO curso_participante ('cp_id', 'cp_curso', 'cp_participante') VALUES ($cp_id, $cp_curso, $cp_participante)";
+		$this->set_query();
+	}
+
+	public function read( $cp_id = '' ) {
+		$this->query = ($cp_id != '')
+    ?"SELECT c.curso_id, c.curso_name, c.curso_fecha, p.participante_id, p.nombre, p.apellido, p.cedula, p.correo, p.direccion
+		FROM cursos AS c
+		INNER JOIN curso_participante AS cp ON c.curso_id=cp.cp_curso
+		INNER JOIN participantes as p ON cp.cp_participante=p.participante_id
+		WHERE c.curso_id = $cp_id
+		ORDER BY p.apellido"
+
+    :"SELECT c.curso_id, c.curso_name, c.curso_fecha, p.participante_id, p.nombre, p.apellido, p.cedula, p.correo, p.direccion
+		FROM cursos AS c
+		INNER JOIN curso_participante AS cp ON c.curso_id=cp.cp_curso
+		INNER JOIN participantes as p ON cp.cp_participante=p.participante_id";
+
+		$this->get_query();
+
+		// $num_rows = count($this->rows);
+
+		$data = array();
+
+		foreach ($this->rows as $key => $value) {
+			array_push($data, $value);
+		}
+
+		return $data;
+	}
+
+	public function update( $cp_data = array() ) {
+		foreach ($cp_data as $key => $value) {
+			$$key = $value;
+		}
+
+		$this->query = "UPDATE curso_participante SET cp_id = $cp_id, cp_curso = $cp_curso, cp_participante = $cp_participante WHERE cp_id = $cp_id";
+		$this->set_query();
+	}
+
+	public function delete( $cp_id = '' ) {
+		$this->query = "DELETE FROM curso_participante WHERE cp_id = $cp_id";
+		$this->set_query();
+	}
+
+}
+
+?>
--- views/info_curso.php
+++ views/info_curso.php
@@ -1,12 +1,59 @@
+<div align="center">
 <?php
 
 $cursos = new CursosModel();
 $cursos_datos = $cursos->read($_POST['c']);
-$template = '';
-
+$cp = new CP_Model();
+$cp_datos = $cp->read($_POST['c']);
+//var_dump ($cp_datos);
+$template = '<table>
+  <tr>
+    <td>Nombre</td>
+    <td>Apellido</td>
+    <td>Cedula</td>
+    <td>Correo</td>
+    <td>Direccion</td>
+    <td colspan="2">opciones</td>
+  </tr>';
+  foreach ($cp_datos as $key) {
+    $template .= '
+    <tr>
+      <td>'. $key['nombre'] .'</td>
+      <td>'. $key['apellido'] .'</td>
+      <td>'. $key['cedula'] .'</td>
+      <td>'. $key['correo'] .'</td>
+      <td>'. $key['direccion'] .'</td>
+      <td>Editar</td>
+      <td>Eliminar</td>
+    </tr>';
+  }
+  $template .= '
+  <tr>
+    <td colspan="7" align="center">Agregar</td>
+  </tr>
+</table>';
+  printf($template);
 ?>
-<div align="center">
-  <h2>Titulo Curso</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!--<h2>Titulo Curso</h2>
 </div>
 <div align="left">
   <form method="post">
@@ -16,4 +63,4 @@ $template = '';
     Fecha:<input type="date" name="" value=""><br>
     <input type="submit" name="" value="Añadir">
   </form>
-</div>
+</div>-->
